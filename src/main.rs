0efa84ea513478d37d70f647315725c26952693b
Ussama Azam <nuclearoreo@gmail.com>
2022-09-22 15:30:47 -0400

    Testing out rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Amazing I got this!!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
