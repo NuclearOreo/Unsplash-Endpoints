88eb0fa072fee30f9b526132ce099b4349fd885b
Ussama Azam <nuclearoreo@gmail.com>
2022-09-26 14:23:38 -0400

    Testing Unsplash API

--- src/unsplash.rs
+++ src/unsplash.rs
@@ -1,20 +1,27 @@
+use reqwest::header::AUTHORIZATION;
 use reqwest::Client;
 use std::error::Error;
 
+#[derive(Debug)]
 pub struct UnsplashClient {
     client: Client,
+    auth: String,
 }
 
 impl UnsplashClient {
     pub fn new() -> UnsplashClient {
         let client = reqwest::Client::new();
-        UnsplashClient { client }
+        let auth = "".to_string();
+        UnsplashClient { client, auth }
     }
 
     pub async fn get_page(&self) -> Result<String, Box<dyn Error>> {
+        let auth = self.auth.clone();
+
         Ok(self
             .client
-            .get("https://www.google.com/")
+            .get("https://api.unsplash.com/users/ussamaazam/photos")
+            .header(AUTHORIZATION, auth)
             .send()
             .await?
             .text()
