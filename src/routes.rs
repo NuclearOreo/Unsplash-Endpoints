03f7d8aff2f74d9452d804eea6279a241df053ca
Ussama Azam <nuclearoreo@gmail.com>
2022-10-06 13:40:47 -0400

    Replace match with unwrap_or

--- src/routes.rs
+++ src/routes.rs
@@ -3,15 +3,8 @@ use rocket::serde::json::{json, Value};
 
 #[get("/get_photos?<page_number>&<per_page>")]
 pub async fn get_photos(page_number: Option<i32>, per_page: Option<i32>) -> Value {
-    let page_number = match page_number {
-        Some(v) => v,
-        None => 1,
-    };
-
-    let per_page = match per_page {
-        Some(v) => v,
-        None => 10,
-    };
+    let page_number = page_number.unwrap_or(1);
+    let per_page = per_page.unwrap_or(10);
 
     let unsplash_client = UnsplashClient::new();
     let result = unsplash_client.get_photos(page_number, per_page).await;
