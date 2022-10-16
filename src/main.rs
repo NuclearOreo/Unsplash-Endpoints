cad3b3502ed9c8126c0ed355bb3d92154038d365
Ussama Azam <nuclearoreo@gmail.com>
2022-10-16 17:18:03 -0400

    CORS implementation (#5)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod routes;
 mod unsplash;
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
 use routes::get_photos;
 
 #[macro_use]
@@ -11,9 +15,32 @@ fn index() -> &'static str {
     "Service is running 🚀"
 }
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to response",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(CORS)
         .mount("/", routes![index])
         .mount("/unsplash/", routes![get_photos])
 }
